da0c7374e6b74bf3fdf0cde776f3582259671f27
Vincent Sanders <vincesanders@hotmail.com>
2020-01-31 10:46:51 -0600

    Added edit functionality to colors in ColorList.js

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from '../utils';
+import { createPortal } from "react-dom";
 
 const initialColor = {
   color: "",
@@ -21,6 +22,16 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+          updateColors(colors.map(color => {
+            if (color.id === colorToEdit.id) {
+              return res.data;
+            }
+            return color;
+          }));
+      }).catch(err => console.log(err));
   };
 
   const deleteColor = color => {
