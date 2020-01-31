860404244530ec157e80223b79b1785e10829082
Vincent Sanders <vincesanders@hotmail.com>
2020-01-31 10:31:27 -0600

    Added Login page (no styling). Fixed a bug with import order in BubblePage that was causing react to throw errors.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const history = useHistory();
+  const [creds, setCreds] = useState({
+      username: '',
+      password: ''
+  });
+
+  const login = e => {
+    e.preventDefault();
+    axios
+        .post('http://localhost:5000/api/login', creds)
+        .then(res => {
+            localStorage.setItem('token', res.data.payload);
+            history.push('/protected');
+        })
+        .catch(err => console.log(err));
+  }
+
+  const handleChange = e => {
+    setCreds({
+        ...creds,
+        [e.target.name]: e.target.value
+    })
+  }
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <form onSubmit={login} >
+        <input type='text' name='username' value={creds.username} onChange={handleChange} />
+        <input type='password' name='password' value={creds.password} onChange={handleChange} />
+        <button>Log in</button>
+    </form>
   );
 };
 
